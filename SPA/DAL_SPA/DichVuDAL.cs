770c7199c475bd0f871c289e24bd40759efc9f60
Shind <duyt7416@gmail.com>
2025-03-29 15:35:52 +0700

    update DV

---
+++ SPA/DAL_SPA/DichVuDAL.cs
@@ -0,0 +1,42 @@
+﻿using DTO_SPA;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace DAL_SPA
+{
+    public class DichVuDAL
+    {
+        List<DichVu> listDichVu;
+
+        public List<DichVu> ListDichVu
+        {
+            get => listDichVu;
+            set => listDichVu = value;
+        }
+
+        public DichVuDAL()
+        {
+            ListDichVu = new List<DichVu>();
+        }
+
+        // Đọc danh sách dịch vụ từ file xml
+        public List<DichVu> docFile(string file)
+        {
+            XmlDocument read = new XmlDocument();
+            read.Load(file);
+            //....
+            return ListDichVu;
+        }
+
+        // Xuất danh sách dịch vụ
+        public void xuatDSDV()
+        {
+            foreach (DichVu dichVu in ListDichVu)
+                dichVu.xuatThongTinDichVu();
+        }
+    }
+}
--- SPA/DTO_SPA/DichVu.cs
+++ SPA/DTO_SPA/DichVu.cs
@@ -3,18 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using static DTO_SPA.DichVu;
 
 namespace DTO_SPA
 {
     public abstract class DichVu
     {
-        public enum LoaiDV { ChamSocSacDep, ChamSocBody, DuongSinh }
+        public enum LoaiDichVuEnum { ChamSocSacDep, ChamSocBody, DuongSinh }
 
         protected string maDichVu;
         protected string tenDichVu;
-        protected string loaiDichVu;
+        protected LoaiDichVuEnum loaiDichVu;
         protected double giaThanh;
+
+        DichVu dichVu;
         public string MaDichVu 
         {  
             get => maDichVu;
@@ -35,17 +36,39 @@ namespace DTO_SPA
                 else tenDichVu = "Massage";
             } 
         }
-        public string LoaiDichVu
+        public LoaiDichVuEnum LoaiDichVu
         {
             get => loaiDichVu;
-            set
+            set => loaiDichVu = value;
+        }
+
+        public static LoaiDichVuEnum NhapLoaiDichVu()
+        {
+            Console.WriteLine("Chọn loại dịch vụ:");
+            Console.WriteLine("1. Chăm sóc sắc đẹp");
+            Console.WriteLine("2. Chăm sóc body");
+            Console.WriteLine("3. Dưỡng sinh");
+
+            while (true)
             {
-                if ((!(string.IsNullOrEmpty(value)) && string.IsNullOrWhiteSpace(value)) &&
-                    (value == "ChamSocSacDep" || value == "ChamSocBody" || value == "DuongSinh"))
-                    loaiDichVu = value;
-                else loaiDichVu = "ChamSocSacDep";  //Mặc định là Chăm sóc sắc đẹp
+                Console.Write("Nhập lựa chọn (1-3): ");
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        return LoaiDichVuEnum.ChamSocSacDep;
+                    case "2":
+                        return LoaiDichVuEnum.ChamSocBody;
+                    case "3":
+                        return LoaiDichVuEnum.DuongSinh;
+                    default:
+                        Console.WriteLine("Lựa chọn không hợp lệ. Vui lòng nhập lại.");
+                        break;
+                }
             }
         }
+
         public double GiaThanh
         {
             get => giaThanh;
@@ -62,7 +85,7 @@ namespace DTO_SPA
 
         }
 
-        public DichVu(string madv, string tendv, string loaidv, double giathanh)
+        public DichVu(string madv, string tendv, LoaiDichVuEnum loaidv, double giathanh)
         {
             MaDichVu = madv;
             TenDichVu = tendv;
@@ -78,6 +101,26 @@ namespace DTO_SPA
             GiaThanh = dv.GiaThanh;
         }
 
+        public void nhapThongTinDichVu()
+        {
+            Console.Write("Nhập mã dịch vụ: ");
+            MaDichVu = Console.ReadLine();
+            Console.Write("Nhập tên dịch vụ: ");
+            TenDichVu = Console.ReadLine();
+            Console.Write("Nhập giá thành: ");
+            GiaThanh = double.Parse(Console.ReadLine());
+            LoaiDichVuEnum loaiDV = NhapLoaiDichVu();
+        }
+
+        public void xuatThongTinDichVu()
+        {
+            Console.WriteLine($"Mã Dịch vụ: {MaDichVu}");
+            Console.WriteLine($"Tên dịch vụ: {TenDichVu}");
+            Console.WriteLine($"Loại dịch vụ: {LoaiDichVu}");
+            Console.WriteLine($"Giá gốc: {GiaThanh:N0}");
+            Console.WriteLine($"Giá sau giảm: {TinhGiaSauGiamGia():N0}");
+        }
+
         // Phương thức trừu tượng - các lớp con phải override
         public abstract double TinhGiaSauGiamGia();
     }
@@ -91,6 +134,11 @@ namespace DTO_SPA
     // Lớp dịch vụ chăm sóc sắc đẹp (không giảm giá)
     public class DichVuChamSocSacDep : DichVu,ICapNhatGia
     {
+        public DichVuChamSocSacDep() : base()
+        {
+            LoaiDichVu = LoaiDichVuEnum.ChamSocSacDep;
+        }
+
         public override double TinhGiaSauGiamGia()
         {
             return GiaThanh; // Không giảm giá
@@ -105,6 +153,10 @@ namespace DTO_SPA
     // Lớp dịch vụ chăm sóc body (giảm 7%)
     public class DichVuChamSocBody : DichVu
     {
+        public DichVuChamSocBody() : base()
+        {
+            LoaiDichVu = LoaiDichVuEnum.ChamSocBody;
+        }
         public override double TinhGiaSauGiamGia()
         {
             return GiaThanh * 0.93; // Giảm 7%
@@ -114,6 +166,10 @@ namespace DTO_SPA
     // Lớp dịch vụ dưỡng sinh (giảm 10%)
     public class DichVuDuongSinh : DichVu
     {
+        public DichVuDuongSinh() : base()
+        {
+            LoaiDichVu = LoaiDichVuEnum.DuongSinh;
+        }
         public override double TinhGiaSauGiamGia()
         {
             return GiaThanh * 0.90; // Giảm 10%
