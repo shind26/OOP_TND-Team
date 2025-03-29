29eee91309a3b964dd71fa85bc0f5244e10376f7
Shind <duyt7416@gmail.com>
2025-03-29 14:28:54 +0700

    update DichVu and KhachHang

---
+++ SPA/DTO_SPA/DichVu.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static DTO_SPA.DichVu;
+
+namespace DTO_SPA
+{
+    public abstract class DichVu
+    {
+        public enum LoaiDV { ChamSocSacDep, ChamSocBody, DuongSinh }
+
+        protected string maDichVu;
+        protected string tenDichVu;
+        protected string loaiDichVu;
+        protected double giaThanh;
+        public string MaDichVu 
+        {  
+            get => maDichVu;
+            set
+            {
+                if (!(string.IsNullOrEmpty(value)) && string.IsNullOrWhiteSpace(value))
+                    maDichVu = value;
+                else maDichVu = "DV000";
+            }
+        }
+        public string TenDichVu 
+        { 
+            get => tenDichVu;
+            set
+            {
+                if (!(string.IsNullOrEmpty(value)) && string.IsNullOrWhiteSpace(value))
+                    tenDichVu = value;
+                else tenDichVu = "Massage";
+            } 
+        }
+        public string LoaiDichVu
+        {
+            get => loaiDichVu;
+            set
+            {
+                if ((!(string.IsNullOrEmpty(value)) && string.IsNullOrWhiteSpace(value)) &&
+                    (value == "ChamSocSacDep" || value == "ChamSocBody" || value == "DuongSinh"))
+                    loaiDichVu = value;
+                else loaiDichVu = "ChamSocSacDep";  //Mặc định là Chăm sóc sắc đẹp
+            }
+        }
+        public double GiaThanh
+        {
+            get => giaThanh;
+            set
+            {
+                if (value > 0)
+                    giaThanh = value;
+                else value = 20000;
+            }
+        }
+
+        public DichVu()
+        {
+
+        }
+
+        public DichVu(string madv, string tendv, string loaidv, double giathanh)
+        {
+            MaDichVu = madv;
+            TenDichVu = tendv;
+            LoaiDichVu = loaidv;
+            GiaThanh = giathanh;
+        }
+
+        public DichVu(DichVu dv)
+        {
+            MaDichVu = dv.MaDichVu;
+            TenDichVu = dv.TenDichVu;
+            LoaiDichVu = dv.LoaiDichVu;
+            GiaThanh = dv.GiaThanh;
+        }
+
+        // Phương thức trừu tượng - các lớp con phải override
+        public abstract double TinhGiaSauGiamGia();
+    }
+
+    //Interface phương thức cập nhật giá
+    public interface ICapNhatGia
+    {
+        void CapNhatGia();
+    }
+
+    // Lớp dịch vụ chăm sóc sắc đẹp (không giảm giá)
+    public class DichVuChamSocSacDep : DichVu,ICapNhatGia
+    {
+        public override double TinhGiaSauGiamGia()
+        {
+            return GiaThanh; // Không giảm giá
+        }
+
+        public void CapNhatGia()
+        {
+            GiaThanh *= 1.03; // Tăng 3%
+        }
+    }
+
+    // Lớp dịch vụ chăm sóc body (giảm 7%)
+    public class DichVuChamSocBody : DichVu
+    {
+        public override double TinhGiaSauGiamGia()
+        {
+            return GiaThanh * 0.93; // Giảm 7%
+        }
+    }
+
+    // Lớp dịch vụ dưỡng sinh (giảm 10%)
+    public class DichVuDuongSinh : DichVu
+    {
+        public override double TinhGiaSauGiamGia()
+        {
+            return GiaThanh * 0.90; // Giảm 10%
+        }
+    }
+}
---
+++ SPA/DTO_SPA/KhachHang.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO_SPA
+{
+    public class KhachHang
+    {
+        private string maKhachHang;
+        private string tenKhachHang;
+        private string soDienThoai;
+        public List<DichVu> danhSachDichVu = new List<DichVu>();
+        public string MaKhachHang 
+        { 
+            get => maKhachHang;
+            set
+            {
+                if (!(string.IsNullOrEmpty(value)) && string.IsNullOrWhiteSpace(value))
+                    maKhachHang = value;
+                else maKhachHang = "KH000";
+            } 
+        }
+        public string TenKhachHang 
+        { 
+            get => tenKhachHang;
+            set
+            {
+                if (!(string.IsNullOrEmpty(value)) && string.IsNullOrWhiteSpace(value))
+                    maKhachHang = value;
+                else maKhachHang = "Anonymous";
+            } 
+        }
+        public string SoDienThoai 
+        { 
+            get => soDienThoai;
+            set
+            {
+                if (value.Length == 10 && value.StartsWith("0") && !(string.IsNullOrEmpty(value)) && string.IsNullOrWhiteSpace(value))
+                    maKhachHang = value;
+                else maKhachHang = "0000000000";
+            } 
+        }
+        public List<DichVu> DanhSachDichVu 
+        { 
+            get => danhSachDichVu;
+            set => danhSachDichVu = value;
+        }
+
+        public KhachHang()
+        {
+
+        }
+    }
+}
